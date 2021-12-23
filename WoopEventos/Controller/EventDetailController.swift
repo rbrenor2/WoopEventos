51b03d5d952f9ec5488dd07cfc28c9033989c439
brenorios-br2digital <74222632+brenorios-br2digital@users.noreply.github.com>
2021-12-23 15:17:30 -0300

    Setting up EventDetail UI

--- WoopEventos/Controller/EventDetailController.swift
+++ WoopEventos/Controller/EventDetailController.swift
@@ -6,15 +6,17 @@
 //
 
 import UIKit
+import SDWebImage
+import MapKit
 
 class EventDetailController: UIViewController {
     // MARK: - Properties
     let imageView: UIImageView = {
         let iv = UIImageView()
         iv.backgroundColor = .lightGreen
-        iv.contentMode = .scaleAspectFit
+        iv.contentMode = .scaleAspectFill
         iv.clipsToBounds = true
-        //        iv.sd_setImage(with: URL(string: "https://lproweb.procempa.com.br/pmpa/prefpoa/seda_news/usu_img/Papel%20de%20Parede.png"))
+                iv.sd_setImage(with: URL(string: "https://lproweb.procempa.com.br/pmpa/prefpoa/seda_news/usu_img/Papel%20de%20Parede.png"))
         return iv
     }()
     
@@ -40,19 +42,42 @@ class EventDetailController: UIViewController {
         return label
     }()
     
-    let mapView: UIImageView = {
-        let iv = UIImageView()
-        iv.backgroundColor = .mainPurple
-        iv.contentMode = .scaleAspectFit
-        iv.clipsToBounds = true
-        //        iv.sd_setImage(with: URL(string: "https://lproweb.procempa.com.br/pmpa/prefpoa/seda_news/usu_img/Papel%20de%20Parede.png"))
-        return iv
+    let mapView: MKMapView = {
+        let mapView = MKMapView()
+        
+        mapView.mapType = MKMapType.standard
+        mapView.isZoomEnabled = true
+        mapView.isScrollEnabled = true
+        
+        
+        return mapView
     }()
     
-    let shareButton: UIButton = Utilities().actionButton(withIcon: .fontAwesomeSolid(.share), size: 42, color: .mainPurple)
+    let shareButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage.init(systemName: "square.and.arrow.up"), for: .normal)
+        return button
+    }()
+
+    let checkinButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .lightPurple
+        button.setDimensions(width: 84, height: 32)
+        button.layer.cornerRadius = 32/2
+        button.setTitle("Check-in", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 12)
+        button.contentEdgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+
+        return button
+    }()
     
-    let checkinButton: UIButton = Utilities().actionButton(withIcon: .fontAwesomeSolid(.checkCircle), size: 42, color: .mainPurple)
-    
+    lazy var scrollView: UIScrollView = {
+        let scroll = UIScrollView(frame: self.view.bounds)
+        scroll.contentSize = CGSize(width: 100, height: scroll.frame.height + 900)
+        scroll.showsHorizontalScrollIndicator = true
+        scroll.bounces = true
+        return scroll
+    }()
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
@@ -66,27 +91,38 @@ class EventDetailController: UIViewController {
         view.addSubview(imageView)
         imageView.anchor(top: view.topAnchor, left: view.leftAnchor, right: view.rightAnchor, width: view.frame.width, height: 200)
         
-        let actionStack = UIStackView(arrangedSubviews: [shareButton, checkinButton])
-        actionStack.axis = .horizontal
-        actionStack.spacing = 4
-        view.addSubview(actionStack)
-        actionStack.anchor(top: imageView.bottomAnchor, right: view.rightAnchor, paddingTop: 12, paddingRight: 12)
+        view.addSubview(shareButton)
+        shareButton.anchor(top: imageView.bottomAnchor, right: view.rightAnchor, paddingTop: 12, paddingRight: 12)
         
-        let scrollView = UIScrollView()
+        view.addSubview(checkinButton)
+        checkinButton.anchor(top: imageView.bottomAnchor, right: view.rightAnchor, paddingTop: 12, paddingRight: 12)
+        
         view.addSubview(scrollView)
-        scrollView.anchor(top: imageView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 18, paddingLeft: 12, paddingRight: 12, height: view.frame.height - imageView.frame.height - 18)
+        scrollView.anchor(top: imageView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 60, paddingLeft: 20, paddingRight: 20, width: view.frame.width, height: view.frame.height)
         
         let descriptionStack = Utilities().infoStack(withTitle: titleDescriptionLabel, views: [descriptionLabel], direction: .vertical)
         scrollView.addSubview(descriptionStack)
-        descriptionStack.anchor(top: scrollView.topAnchor, left: scrollView.leftAnchor, right: scrollView.rightAnchor, paddingTop: 18, paddingLeft: 12, paddingRight: 12)
+        descriptionStack.anchor(top: scrollView.topAnchor, left: scrollView.leftAnchor, right: scrollView.rightAnchor, paddingTop: 18, width: scrollView.frame.width - 500)
         
         let locationStack = Utilities().infoStack(withTitle: titleLocationLabel, views: [mapView], direction: .vertical)
-        mapView.setDimensions(width: view.frame.width, height: 200)
+        mapView.setDimensions(width: scrollView.frame.width, height: 200)
         scrollView.addSubview(locationStack)
-        locationStack.anchor(top: descriptionStack.bottomAnchor, left: scrollView.leftAnchor, right: scrollView.rightAnchor, paddingTop: 18, paddingLeft: 12, paddingRight: 12)
+        locationStack.anchor(top: descriptionStack.bottomAnchor, left: scrollView.leftAnchor, right: scrollView.rightAnchor, paddingTop: 18, width: scrollView.frame.width - 500)
         
-       
+        let leftMargin:CGFloat = 10
+        let topMargin:CGFloat = 60
+        let mapWidth:CGFloat = view.frame.width-20
+        let mapHeight:CGFloat = 300
         
+        mapView.frame = CGRect(x: leftMargin, y: topMargin, width: mapWidth, height: mapHeight)
+        
+        mapView.mapType = MKMapType.standard
+        mapView.isZoomEnabled = true
+        mapView.isScrollEnabled = true
+        
+        // Or, if needed, we can position map in the center of the view
+        mapView.center = view.center
+
     }
     
     // MARK: - API
