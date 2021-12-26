96012ec919f8355997169a50cb9b9aa5037814ab
brenorios-br2digital <74222632+brenorios-br2digital@users.noreply.github.com>
2021-12-25 23:56:59 -0300

    Making first controller a nav controller

--- WoopEventos/SceneDelegate.swift
+++ WoopEventos/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: scene)
-        window?.rootViewController = EventListController()
+        window?.rootViewController = UINavigationController(rootViewController: EventListController()) 
         window?.makeKeyAndVisible()
     }
 
